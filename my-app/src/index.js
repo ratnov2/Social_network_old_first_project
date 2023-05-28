732c66465ffbcd980105c5619270e61e4e07a68c
ratnov2 <73044570+ratnov2@users.noreply.github.com>
2023-05-28 18:41:03 +0300

    git init

---
+++ my-app/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import store from './redux/redux-store'
+import {Provider} from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store ={store}>   
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+reportWebVitals();
